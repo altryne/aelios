e5e4547f5b4d1f243e4586ada3ae041ca22050d3
altryne <git@alexw.me>
2011-08-15 01:33:04 +0300

    SHITLOAD OF STUFF ADDED, CANVAS AND SHIT

--- index.php
+++ index.php
@@ -2,7 +2,14 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <!-- Prevent scaling -->
+    <meta name="viewport" content = "user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+
+    <!-- Eliminate url and button bars if added to home screen -->
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+
+    <!-- Choose how to handle the phone status bar -->
+    <meta names="apple-mobile-web-app-status-bar-style" content="black-translucent" />
     <title>Aelios html5 clone</title>
     <link rel="stylesheet" type="text/css" href="css/style.css"/>
     <script type="text/javascript" src="../live.js"></script>
@@ -12,8 +19,9 @@
 </head>
 <body>
 <div id="mainmap" style="width:100%; height:100%">
-    
+
 </div>
+<div id="template">
 <div id="title">
     <div class="titleCont">
         <div id='location'></div>
@@ -31,6 +39,9 @@
 <div id="mylocation"></div>
 <div id="search"></div>
 <div id="marker">
+    <canvas id="dayLightCanvas" width="320" height="320"></canvas>
+    <div id="glass"></div>
 </div>
+    </div>
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -4,20 +4,22 @@
  * Date: 25/5/11
  * Time: 03:42
  */
-dayOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var mapLoaded = 0;
+var dayOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 $(document).ready(function(){
     var map;
     var geocoder;
     function updateCurrentLocation(curLoc){
         $('#loader').fadeIn();
+        $('#template').removeClass('drag');
         if (geocoder) {
             var latlngStr = String(curLoc);
             latlngStr = latlngStr.substring(1,latlngStr.length-1);
             latlngStr = latlngStr.split(",");
             var lat = parseFloat(latlngStr[0]);
             var lng = parseFloat(latlngStr[1]);
-             $.getJSON("http://ws.geonames.org/timezoneJSON?callback=?",
+            var jqxhr = $.getJSON("http://ws.geonames.org/timezoneJSON?callback=?",
               {
                   'uID': 1,
                   lat:lat,
@@ -28,6 +30,8 @@ $(document).ready(function(){
                       var _date = data.time.split(' ')[0];
                       day = new Date(_date.split('-').join('/'));
                       $('#time').html(data.time.split(' ')[1]);
+                      //split datetime to time
+                      updateLightHours(data.sunrise.split(' ')[1],data.sunset.split(' ')[1]);
                       $('#date').html(dayOfWeek[day.getDay()] + ', ' + months[day.getMonth()] + ' ' + day.getDate() + ', ' + day.getFullYear());
                   }
                   if (data.countryName) {
@@ -39,16 +43,57 @@ $(document).ready(function(){
             );
             var latlng = new google.maps.LatLng(lat, lng);
               geocoder.geocode({'latLng': latlng}, function(results, status) {
-
                 if (status == google.maps.GeocoderStatus.OK) {
                     var place = results[results.length - 2].formatted_address.split(',');
                     place.pop();
+                    if(!place.length){
+                        place = results[1].formatted_address.split(',');
+                        place.pop();
+                        if(!place.length){
+                            $('#location').html('Somewhere...');
+                        }
+                    }
                     $('#location').html(place.join(','));
                 } else {
-                   $('#location').html('Somwhere...');
+                   $('#location').html('Somewhere...');
                 }
               });
             }
+    }
+    dragstarted = function(){
+        $('#template').addClass('drag');
+    }
+    updateLightHours = function(beginTime,endTime) {
+        var degOffset = 90 //deg to start from bottom center
+        var beginTime = beginTime.split(':');
+        var beginTimeInMinutes = parseInt(beginTime[0], 10) * 60 + parseInt(beginTime[1], 10);
+        var endTime = endTime.split(':');
+        var endTimeInMinutes = parseInt(endTime[0], 10) * 60 + parseInt(endTime[1], 10);
+        //each minute == 0.25 deg
+        var beginDegFromTime = beginTimeInMinutes * .25 - degOffset;
+        var endDegFromTime = endTimeInMinutes * .25 - degOffset;
+        drawLight(beginDegFromTime,endDegFromTime);
+    }
+    drawLight = function(beginDeg,endDeg){
+        canvas = document.getElementById("dayLightCanvas");
+        context = canvas.getContext("2d");
+        canvas.width = canvas.width;
+        context.lineWidth = 44;
+        centerX = centerY = canvas.offsetWidth / 2;
+//        centerY = canvas.offsetHeight / 2;
+        radius = canvas.offsetWidth / 2 - context.lineWidth/2;
+        //one rad = (PI/180) * deg
+        startingAngle = (Math.PI / 180) * beginDeg;
+        endingAngle = (Math.PI / 180) * endDeg;
+        counterclockwise = false;
+
+        context.arc(centerX, centerY, radius, startingAngle, endingAngle, true);
+        context.strokeStyle = "black"; // line color
+        context.stroke();
+        context.beginPath();
+        context.arc(centerX, centerY, radius, startingAngle, endingAngle, counterclockwise);
+        context.strokeStyle = "white"; // line color
+        context.stroke();
     }
     function createMap() {
         var myLatlng = new google.maps.LatLng(51.5085932, -0.1247547);
@@ -67,6 +112,23 @@ $(document).ready(function(){
         map = new google.maps.Map(document.getElementById("mainmap"), myOptions);
         google.maps.event.addListener(map, 'dragend', function() {
             updateCurrentLocation(map.getCenter());
+        });
+        google.maps.event.addListener(map, 'dragstart', dragstarted);
+        google.maps.event.addListener(map, 'tilesloaded', function() {
+            if(mapLoaded) return false;
+            mapLoaded = true;
+//            var html = $('#template').css('display','block');
+//            $(html).appendTo('#mainmap>div>div:first-child>div>div');
+        });
+        $('#mylocation').bind('click',function(){
+            navigator.geolocation.getCurrentPosition(function(data){
+                var myLatlng = new google.maps.LatLng(data.coords.latitude, data.coords.longitude);
+                map.setOptions({
+                    zoom : 10
+                });
+                map.panTo(myLatlng);
+                updateCurrentLocation(map.getCenter())
+            });
         });
         updateCurrentLocation(map.getCenter());
     }
